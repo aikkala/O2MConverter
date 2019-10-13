1aaba0119830c072a8ec41b32256a23ee6705197
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2019-10-13 11:16:26 +0100

    Add Utils module

---
+++ Utils.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def is_nested_field(d, field, nested_fields):
+    # Check if field is in the given dictionary after nested fields
+    if len(nested_fields) > 0:
+        if nested_fields[0] in d:
+            return is_nested_field(d[nested_fields[0]], field, nested_fields[1:])
+        else:
+            return False
+    else:
+        if field in d:
+            return True
+        else:
+            return False
+
+
+def create_rotation_matrix(axis, rad):
+    R = np.eye(4, 4)
+
+    # Make sure axis is a unit vector
+    axis = axis / np.linalg.norm(axis)
+
+    l = axis[0]
+    m = axis[1]
+    n = axis[2]
+
+    # Create the rotation matrix
+    R[0, 0] = l*l*(1-np.cos(rad)) + np.cos(rad)
+    R[0, 1] = m*l*(1-np.cos(rad)) - n*np.sin(rad)
+    R[0, 2] = n*l*(1-np.cos(rad)) + m*np.sin(rad)
+    R[1, 0] = l*m*(1-np.cos(rad)) + n*np.sin(rad)
+    R[1, 1] = m*m*(1-np.cos(rad)) + np.cos(rad)
+    R[1, 2] = n*m*(1-np.cos(rad)) - l*np.sin(rad)
+    R[2, 0] = l*n*(1-np.cos(rad)) - m*np.sin(rad)
+    R[2, 1] = m*n*(1-np.cos(rad)) + l*np.sin(rad)
+    R[2, 2] = n*n*(1-np.cos(rad)) + np.cos(rad)
+
+    return R
+
+
+def create_translation_vector(axis, l):
+    t = np.zeros(shape=(3,))
+    if axis[0] == 1 and axis[1] == 0 and axis[2] == 0:
+        t[0] = l
+    elif axis[0] == 0 and axis[1] == 1 and axis[2] == 0:
+        t[1] = l
+    elif axis[0] == 0 and axis[1] == 0 and axis[2] == 1:
+        t[2] = l
+    else:
+        raise "Was not expecting this axis"
+    return t
+
+
+def create_translation_matrix(axis, l):
+    T = np.eye(4, 4)
+    t = create_translation_vector(axis, l)
+    T[0:3, 3] = t
+    return T
+
+def create_symmetric_matrix(vec):
+    # Assume vec is a vector of upper triangle values for matrix of size 3x3 (xx,yy,zz,xy,xz,yz)
+    matrix = np.diag(vec[0:3])
+    matrix[0, 1] = vec[3]
+    matrix[0, 2] = vec[4]
+    matrix[1, 2] = vec[5]
+    return matrix + matrix.T - np.diag(matrix.diagonal())
