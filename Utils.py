a180e752edcf01730aabd63021a6b1205f075175
Aleksi Ikkala <aleksi.ikkala@aalto.fi>
2020-04-06 09:52:35 +0300

    Move functions into Utils

--- Utils.py
+++ Utils.py
@@ -3,6 +3,8 @@ from pyquaternion import Quaternion
 import math
 import pandas as pd
 import matplotlib.pyplot as pp
+import mujoco_py
+import skvideo.io
 
 
 def is_nested_field(d, field, nested_fields):
@@ -172,7 +174,7 @@ def reindex_dataframe(df, timestep, last_timestamp=None):
     return new_df
 
 
-def estimate_joint_error(reference, simulated, joint_names=None, timesteps=None, plot=False):
+def estimate_joint_error(reference, simulated, joint_names=None, timesteps=None, plot=False, output_file=None):
 
     # Reference and simulated need to be same shape
     if reference.shape != simulated.shape:
@@ -183,22 +185,142 @@ def estimate_joint_error(reference, simulated, joint_names=None, timesteps=None,
     errors = np.empty((reference.shape[1],))
 
     if plot:
-        _, axes = pp.subplots(reference.shape[1], 1)
+        fig, axes = pp.subplots(reference.shape[1], 1, figsize=(10, 18))
         if timesteps is None:
             axes[-1].set_xlabel('Time (indices)')
+            timesteps = np.arange(0, reference.shape[0])
         else:
             axes[-1].set_xlabel('Time (seconds)')
 
     # Get abs sum of difference between reference joint and simulated joint
     for idx in range(reference.shape[1]):
         e = reference[:, idx] - simulated[:, idx]
-        errors[idx] = np.sum(np.abs(e))
+        #errors[idx] = np.sum(np.abs(e))
+        errors[idx] = np.matmul(e, e)
         if plot:
-            if timesteps is None:
-                timesteps = np.arange(0, reference.shape[0])
             axes[idx].plot(timesteps, e)
             axes[idx].plot(np.array([timesteps[0], timesteps[-1]]), np.array([0, 0]), 'k--')
             if joint_names is not None:
                 axes[idx].set_ylabel(joint_names[idx])
 
+    # Save the file if output_file is defined
+    if plot and output_file is not None:
+        fig.savefig(output_file)
+        pp.close(fig)
+
     return errors
+
+
+def check_muscle_order(model, data):
+
+    # Make sure muscles are in the same order
+    for run_idx in range(len(data)):
+        if data[run_idx]["muscle_names"] != list(model.actuator_names):
+            print("Muscles are in incorrect order, fix this")
+            raise NotImplementedError
+
+
+def get_target_state_indices(model, env):
+
+    # Map mujoco joints to target joints
+    target_state_indices = np.empty(len(env.target_states,), dtype=int)
+    for idx, target_state in enumerate(env.target_states):
+        target_state_indices[idx] = model.joint_names.index(target_state)
+
+    return target_state_indices
+
+
+def get_initial_states(model, env):
+
+    # If there are initial states, reorder them according to model joints
+    initial_states = None
+
+    if env.initial_states is not None:
+        qpos = np.zeros(len(model.joint_names),)
+        qvel = np.zeros(len(model.joint_names),)
+        ctrl = np.zeros(len(model.actuator_names),)
+
+        # Get qpos and qvel for joints
+        if "joints" in env.initial_states:
+            for state in env.initial_states["joints"]:
+                if state in model.joint_names:
+                    idx = model.joint_names.index(state)
+                    if "qpos" in env.initial_states["joints"][state]:
+                        qpos[idx] = env.initial_states["joints"][state]["qpos"]
+                    if "qvel" in env.initial_states["joints"][state]:
+                        qvel[idx] = env.initial_states["joints"][state]["qvel"]
+
+        # Get activations for actuators
+        if "actuators" in env.initial_states:
+            for actuator in env.initial_states["actuators"]:
+                idx = model.actuator_names.index(actuator)
+                ctrl[idx] = env.initial_states["actuators"][actuator]
+
+        initial_states = {"qpos": qpos, "qvel": qvel, "ctrl": ctrl}
+
+    return initial_states
+
+
+def initialise_simulation(sim, timestep, initial_states=None):
+
+    # Set timestep
+    sim.model.opt.timestep = timestep
+
+    # Reset sim
+    sim.reset()
+
+    # Set initial states
+    if initial_states is not None:
+        if "qpos" in initial_states:
+            sim.data.qpos[:] = initial_states["qpos"]
+        if "qvel" in initial_states:
+            sim.data.qvel[:] = initial_states["qvel"]
+        if "ctrl" in initial_states:
+            sim.data.ctrl[:] = initial_states["ctrl"]
+
+        # We might need to call forward to make sure everything is set properly after setting qpos (not sure if required)
+        sim.forward()
+
+
+def run_simulation(sim, controls, viewer=None, output_video_file=None):
+
+    qpos = np.empty((len(controls), len(sim.model.joint_names)))
+
+    # For recording / viewing purposes
+    imgs = []
+    width = 1200
+    height = 1200
+
+    # We assume there's one set of controls for each timestep
+    for t in range(len(controls)):
+
+        # Set muscle activations
+        sim.data.ctrl[:] = controls[t, :]
+
+        # Forward the simulation
+        sim.step()
+
+        # Get joint positions
+        qpos[t, :] = sim.data.qpos
+
+        if viewer is not None:
+            if output_video_file is None:
+                viewer.render()
+            else:
+                viewer.render(width, height, sim.model._camera_name2id["for_testing"])
+                imgs.append(np.flipud(viewer.read_pixels(width, height, depth=False)))
+
+    if viewer is not None and output_video_file is not None:
+
+        # Get writer
+        writer = skvideo.io.FFmpegWriter(output_video_file,
+                                         inputdict={"-s": "{}x{}".format(width, height),
+                                                    "-r": str(1/sim.model.opt.timestep)})
+        # Write the video
+        for img in imgs:
+            writer.writeFrame(img)
+
+        # Close writer
+        writer.close()
+
+    return qpos
